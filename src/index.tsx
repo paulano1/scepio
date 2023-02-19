49ea8fde2d88af13d5543dbd96bf958f8f125777
Anomitro Paul <paulano@amazon.com>
2023-02-18 23:55:32 -0500

    added notebook

--- src/index.tsx
+++ src/index.tsx
@@ -14,7 +14,7 @@ root.render(
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://cra.link/PWA
+// Learn more about service workers: https://cra.link/PWAhmmm
 serviceWorkerRegistration.unregister();
 
 // If you want to start measuring performance in your app, pass a function
